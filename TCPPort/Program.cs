d2b6594aad9d10037a908502457b7dedc234f0c7
xiong-ang <xiong.ang@foxmail.com>
2018-09-25 19:42:06 +0800

    First commit

---
+++ TCPPort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<int> ports = GetTCPConnectorPorts();
+            List<int> ports = GetTCPListenerPorts();
+
+            ports.ForEach( p => {
+                Console.WriteLine(p);
+            });
+        }
+
+        static List<int> GetTCPConnectorPorts()
+        {
+            List<int> ports = new List<int>();
+
+            var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnectInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+            tcpConnectInfoArray.ToList().ForEach(p => ports.Add(p.LocalEndPoint.Port));
+
+            return ports;
+        }
+
+        static List<int> GetTCPListenerPorts()
+        {
+            List<int> ports = new List<int>();
+
+            var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpListenInfoArray = ipGlobalProperties.GetActiveTcpListeners();
+
+            tcpListenInfoArray.ToList().ForEach(p => ports.Add(p.Port));
+
+            return ports;
+        }
+    }
+}
